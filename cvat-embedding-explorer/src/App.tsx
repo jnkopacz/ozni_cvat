4d58d27f3c107f180d5153714d39f55a17e1f36b
Justin <jnkopacz@gmail.com>
2025-05-29 14:52:10 -0600

    restyling embedding ui

--- cvat-embedding-explorer/src/App.tsx
+++ cvat-embedding-explorer/src/App.tsx
@@ -1,7 +1,7 @@
 // src/App.tsx
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { Layout } from 'antd';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import { Layout, Button } from 'antd';
 import ProjectsList from './components/ProjectsList/ProjectsList';
 import ClusterExplorer from './components/ClusterExplorer/ClusterExplorer';
 import './styles/index.scss';
@@ -13,7 +13,24 @@ const App: React.FC = () => {
     <Router>
       <Layout className="cvat-layout">
         <Header className="cvat-header">
-          <div className="cvat-logo">CVAT Embedding Explorer</div>
+          <div className="cvat-left-header">
+            <div className="cvat-logo-icon">
+              <img src="/logo.svg" alt="CVAT Logo" />
+              <img
+                src="/logo_ozni_black.png"
+                alt="OZNI Logo"
+                style={{ marginLeft: '10px', height: '23px' }}
+              />
+            </div>
+            <Button
+              className="cvat-header-button"
+              type="link"
+              component={Link}
+              to="/"
+            >
+              Object Embedding Explorer
+            </Button>
+          </div>
         </Header>
         <Layout className="cvat-workspace">
           <Content className="cvat-content">
